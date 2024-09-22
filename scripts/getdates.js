4f25e75392c49ffb075748962fbdf56b60d46761
jediyeap <yeapdick@aol.com>
2024-09-22 20:14:06 +0800

    Week 01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,2 @@
-const year = document.querySelector("#currentyear");
-
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
